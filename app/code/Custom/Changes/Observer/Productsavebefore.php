2f45343a0cf9ab7799dcf8381d15c748c6d7793c
developer@kintec.com <developer@kintec.com>
2017-06-24 04:57:06 -0700

    on product save skipped auto color set functionality if any value is set for it.

--- app/code/Custom/Changes/Observer/Productsavebefore.php
+++ app/code/Custom/Changes/Observer/Productsavebefore.php
@@ -16,49 +16,67 @@ class Productsavebefore implements ObserverInterface
         	if(!is_null($_product->getColor())){
         		$_product->setColor('');
         	}
-        	return $this;
+        	
         }
-        $colour = $attr->getSource()->getOptionText($_product->getColour());
+        else
+        {
+        	if($_product->getColor()==''){
+        		$colour = $attr->getSource()->getOptionText($_product->getColour());
 
-        $colors = [
-		 'red'=>'Red',
-		 'pink'=>'Pink',
-		 'orange'=>'Orange',
+		        $colors = [
+				 'red'=>'Red',
+				 'pink'=>'Pink',
+				 'orange'=>'Orange',
+				 
+				 'purple'=>'Purple',
+				 'grey'=>'Grey',
+				 'green'=>'Green',
+				 'blue'=>'Blue',
+				 
+				 'silver'=>'Silver',
+				 'yellow'=>'Yellow',
+				 'brown'=>'Brown',
+				 'maroon'=>'Maroon',
+				 'violet'=>'Violet',
+				 'chocolate'=>'Chocolate',
+				 'orchid'=>'Orchid',
+				 'gold'=>'Gold',
+				 'black'=>'Black',
+				 'white'=>'White',
+				];
+				$productColor = 'white';
+				foreach($colors as $color=>$code){
+					if(strpos(strtolower($colour),$color)!==false){
+						$productColor = $color;
+
+						break;
+					}
+				}
+
+				$attr1 = $objectManager->get('\Magento\Catalog\Model\Product')->getResource()->getAttribute('color');
 		 
-		 'purple'=>'Purple',
-		 'grey'=>'Grey',
-		 'green'=>'Green',
-		 'blue'=>'Blue',
-		 
-		 'silver'=>'Silver',
-		 'yellow'=>'Yellow',
-		 'brown'=>'Brown',
-		 'maroon'=>'Maroon',
-		 'violet'=>'Violet',
-		 'chocolate'=>'Chocolate',
-		 'orchid'=>'Orchid',
-		 'gold'=>'Gold',
-		 'black'=>'Black',
-		 'white'=>'White',
-		];
-		$productColor = 'white';
-		foreach($colors as $color=>$code){
-			if(strpos(strtolower($colour),$color)!==false){
-				$productColor = $color;
-
-				break;
-			}
-		}
-
-		$attr1 = $objectManager->get('\Magento\Catalog\Model\Product')->getResource()->getAttribute('color');
- 
-		foreach($colors as $color=>$code){
-			$id = $attr1->getSource()->getOptionId($code);
-			$colors[$color] = $id;
-		}
-        $_product->setColor($colors[$productColor]);
+				foreach($colors as $color=>$code){
+					$id = $attr1->getSource()->getOptionId($code);
+					$colors[$color] = $id;
+				}
+		        $_product->setColor($colors[$productColor]);
+        	}
+	        
+	    }
         //var_dump($_product->getData());die;
         //$_sku=$_product->getSku(); // for sku
+        $brandAttr = $objectManager->get('\Magento\Catalog\Model\Product')->getResource()->getAttribute('brand');
+        $brand = $brandAttr->getSource()->getOptionText($_product->getBrand());
+		$model = $_product->getName();
+		$brand = $brand ? $brand : '';
+        $title = "{$model}, {$brand}. | Kintec";
+		$keyword = "{$model}, {$brand}";
+		$description = "Buy {$model} online at Kintec Footwear + Orthotics. Huge selection of shoes and accessories. Free shipping on orders over $99.";
+		$_product->setMetaTitle($title);
+		if(is_null($_product->getMetaKeyword()) || $_product->getMetaKeyword()==''){
+			$_product->setMetaKeyword($keyword);
+		}
+		$_product->setMetaDescription($description);
 
     }   
 }
