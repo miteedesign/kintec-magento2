7560cf6cbad8adc021911124f339c4552d3ce247
developer@kintec.com <developer@kintec.com>
2017-08-02 10:57:48 -0700

    product save before event

--- app/code/Custom/Changes/Observer/Productsavebefore.php
+++ app/code/Custom/Changes/Observer/Productsavebefore.php
@@ -11,15 +11,61 @@ class Productsavebefore implements ObserverInterface
     	$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
     	$attr = $objectManager->get('\Magento\Catalog\Model\Product')->getResource()->getAttribute('colour');
         $_product = $observer->getProduct();  // you will get product object
-
+        $file = fopen(BP.'/logs.log','a');
+        fwrite($file,$_product->getTypeId());
         if($_product->getTypeId()=='configurable'){
         	if(!is_null($_product->getColor())){
         		$_product->setColor('');
+        	}
+        	if($_product->getId()){
+        		/*
+        		$urlManager = $objectManager->get('Magento\Framework\Url');
+	        	$url = $urlManager->getUrl('changes/swatch/cache',['id'=>$_product->getId(),'force'=>1]);
+		        $ch = curl_init();
+		        fwrite($file,$url);
+		        curl_setopt($ch, CURLOPT_URL, $url);
+		        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+		        curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+		        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+		        $output = curl_exec($ch);
+		        $info = curl_getinfo($ch);
+		         fwrite($file,print_r($info,true));
+		         fclose($file);
+		        curl_close($ch);
+		        */
+		        $renderer = $objectManager->create('Magento\Swatches\Block\Product\Renderer\Listing\Configurable');
+                $renderer->setProduct($_product);
+                $renderer->getListSwatchJs(true);
+                /*
+                $renderer = $objectManager->get('Magento\Swatches\Block\Product\Renderer\Configurable');
+                $renderer->setProduct($_product);
+                $renderer->getListSwatchJs(true); 
+                */
         	}
         	
         }
         else
         {
+        	if($_product->getId()){
+        		$products = $objectManager->create('Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable')->getParentIdsByChild($_product->getId());
+        		if(is_array($products)){
+        			foreach($products as $productId)
+				    {
+				    	$product = $objectManager->create('Magento\Catalog\Model\Product')->load($productId);
+				    	if($product->getTypeId()=='configurable'){
+				    		$renderer = $objectManager->create('Magento\Swatches\Block\Product\Renderer\Listing\Configurable');
+			                $renderer->setProduct($product);
+			                $renderer->getListSwatchJs(true);
+			               /* $renderer = $objectManager->get('Magento\Swatches\Block\Product\Renderer\Configurable');
+			                $renderer->setProduct($product);
+			                $renderer->getListSwatchJs(true); 
+			                */
+				    	}
+				    	
+				    }
+        		}
+			    
+        	}
         	if($_product->getColor()==''){
         		$colour = $attr->getSource()->getOptionText($_product->getColour());
 
