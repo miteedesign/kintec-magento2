c31ae59495cd2bc1d936a172b26c2d21b133db04
developer@kintec.com <developer@kintec.com>
2017-08-02 10:55:59 -0700

    added controlelr for saving comment in session

--- app/code/Custom/Changes/Controller/Comment/Save.php
+++ app/code/Custom/Changes/Controller/Comment/Save.php
@@ -9,7 +9,12 @@ class Save extends \Magento\Framework\App\Action\Action
     public function execute()
     {
     	$checkoutSession = $this->_objectManager->get('Magento\Checkout\Model\Session');
-    	$checkoutSession->setShippingComment($this->getRequest()->getParam('comment'));
+    	if($this->getRequest()->getParam('comment'))
+    		$checkoutSession->setShippingComment($this->getRequest()->getParam('comment'));
+    	if($this->getRequest()->getParam('firstname'))
+    		$checkoutSession->setPickfirst($this->getRequest()->getParam('firstname'));
+    	if($this->getRequest()->getParam('lastname'))
+    		$checkoutSession->setPicklast($this->getRequest()->getParam('lastname'));
        // return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
